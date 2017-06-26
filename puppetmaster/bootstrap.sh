a082904f5fc3a2a73b73fa71dda1cfb931777b96
Billy Lieberman <wlieberman@vmware.com>
2017-06-25 22:09:43 -0500

    Made some edits. Installed manually so I could get a clean pe.conf file. Looks like there is only a small change to it

--- puppetmaster/bootstrap.sh
+++ puppetmaster/bootstrap.sh
@@ -5,21 +5,21 @@ yum install vim
 systemctl stop iptables.service
 useradd pe-puppet
 cp /vagrant/hosts /etc/hosts
-cp /vagrant/puppetmaster/puppet-enterprise-2016.4.2-el-6-x86_64.tar.gz /root
+cp /vagrant/puppetmaster/puppet-enterprise-2016.4.2-el-7-x86_64.tar.gz /root
 cd /root
-tar zxf puppet-enterprise-2016.4.2-el-6-x86_64.tar.gz
-cd puppet-enterprise-2016.4.2-el-6-x86_64
-cp /vagrant/puppetmaster/my_answers ./pe.conf 
-mkdir -p /opt/puppetlabs/server/apps
-mkdir -p /etc/puppetlabs/puppetserver/
-mkdir -p /opt/puppetlabs/server/apps/
-./puppet-enterprise-installer -c /root/puppet-enterprise-2016.4.2-el-6-x86_64/pe.conf
-mkdir /etc/puppetlabs/puppetserver/ssh
-cp /vagrant/puppetmaster/id_rsa /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
-chown pe-puppet:pe-puppet /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
-chmod 600 /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
-cp /vagrant/puppetmaster/id_rsa /root/.ssh
-chmod 600 /root/.ssh/id_rsa
+tar zxf puppet-enterprise-2016.4.2-el-7-x86_64.tar.gz
+cd puppet-enterprise-2016.4.2-el-7-x86_64
+cp /vagrant/puppetmaster/pe.conf ./pe.conf
+#mkdir -p /opt/puppetlabs/server/apps
+#mkdir -p /etc/puppetlabs/puppetserver/
+#mkdir -p /opt/puppetlabs/server/apps/
+./puppet-enterprise-installer -c /root/puppet-enterprise-2016.4.2-el-7-x86_64/pe.conf
+#mkdir /etc/puppetlabs/puppetserver/ssh
+#cp /vagrant/puppetmaster/id_rsa /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+#chown pe-puppet:pe-puppet /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+#chmod 600 /etc/puppetlabs/puppetserver/ssh/id-control_repo.rsa
+#cp /vagrant/puppetmaster/id_rsa /root/.ssh
+#chmod 600 /root/.ssh/id_rsa
 /usr/local/bin/r10k deploy environment -p
 cp -f /vagrant/puppetmaster/hiera.yaml /etc/puppetlabs/puppet/hiera.yaml
 cp -f /vagrant/puppetmaster/autosign.conf /etc/puppetlabs/puppet/autosign.conf
